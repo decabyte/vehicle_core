97d99ec6e0cfe06c0f3818a34f289b6333d530c3
Valerio De Carolis <valerio.decarolis@gmail.com>
2014-12-03 19:26:39 +0000

    update code style and licences

--- README.md
+++ README.md
@@ -1,26 +1,33 @@
 vehicle_core
 ============
 
-This repo holds the ROS package of the Autonomous Vehicle Software (AVS) used within the OSL's vehicles. This package is meant to be python importable from other scripts and packages. It provides a consolidated set of modules, nodes, configuration and launch files to run basic missions with one of the OSL's vehicles. 
+This repo holds the ROS package of the Autonomous Vehicle Software (AVS) used with OSL vehicles. This package is
+meant to be python importable from other scripts and packages. It provides a consolidated set of modules, nodes,
+configuration and launch files to run basic missions with one of the OSL's vehicles.
 
 Guidelines
 ----------
 
-Before attempting any modification to this repo please make sure thar: 
+Before attempting any modification to this repo please make sure thar:
   - you are working on an up-to-date version of the `master` branch
   - the previous working version of the repo has been tagged (`git tag --list` to check the tags)
   - you have considered the option of creating a new branch for your feature (`git checkout -b <branch_name>` to create a new branch), after all this is the recommended approach!
   - you know what are you doing!
   
-Initially this repo is providing packages using the rosbuild build system until the OSL vehicles are migrated to an upgraded version of the ROS. Later the rosbuild support is going to be dropped and the master branch will offer a catkinized package format. The software development follows a couple of principles that have been proven useful during the early development of this project like [Semver][semver], for semantic versioning of releases and tags, [KISS][kiss], as a general guideline to prevent the commit of _huge_ files and _beast_ modules, and, finally, [TDD][tdd], as a principle for testing your code if you want to rely on a more pragmatic approach.
+Initially this repo is providing packages using the `rosbuild` build system until the OSL vehicles are migrated to an
+ upgraded version of the ROS. Later the rosbuild support is going to be dropped and the master branch will offer a
+ catkinized package format. The software development follows a couple of principles that have been proven useful
+ during the early development of this project like [Semver][semver], for semantic versioning of releases and tags,
+ [KISS][kiss], as a general guideline to prevent the commit of _huge_ files and _beast_ modules, and, finally,
+ [TDD][tdd], as a principle for testing your code if you want to rely on a more pragmatic approach.
 
 Requirements
 ------------
-  - ROS HYDRO/INDIGO
-  - Joystick
-  ```
-    $ sudo apt-get install ros-hydro-joystick-drivers
-  ```
+  - A valid ROS installation (version Hydro, Indigo or later is suggested)
+  - ROS joy package from ROS Joystick Drivers (`$ sudo apt-get install ros-<VERSION>-joy`)
+  - Python 2.7+
+  - Numpy 1.8+
+  - Scipy 0.13+
 
 Run Simulator
 ---
@@ -88,8 +95,6 @@ Credits
 -------
 
 This work is part of **Nessie VIII AUV** project: *to boldly go where no robot has gone before!*
-
-![nessie_logo](docs/nessie_tran.png)
 
 
 [semver]: http://semver.org/
--- src/vehicle_core/model/vehicle_model.py
+++ src/vehicle_core/model/vehicle_model.py
@@ -1,21 +1,58 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+# Software License Agreement (BSD License)
+#
+#  Copyright (c) 2014, Ocean Systems Laboratory, Heriot-Watt University, UK.
+#  All rights reserved.
+#
+#  Redistribution and use in source and binary forms, with or without
+#  modification, are permitted provided that the following conditions
+#  are met:
+#
+#   * Redistributions of source code must retain the above copyright
+#     notice, this list of conditions and the following disclaimer.
+#   * Redistributions in binary form must reproduce the above
+#     copyright notice, this list of conditions and the following
+#     disclaimer in the documentation and/or other materials provided
+#     with the distribution.
+#   * Neither the name of the Heriot-Watt University nor the names of
+#     its contributors may be used to endorse or promote products
+#     derived from this software without specific prior written
+#     permission.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+#  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+#  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+#  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+#  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+#  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+#  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+#  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+#  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+#  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+#  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+#  POSSIBILITY OF SUCH DAMAGE.
+#
+#  Original authors:
+#   Valerio De Carolis, Marian Andrecki, Corina Barbalata, Gordon Frost
+
 """
-NOTES:
-    The buoyancy term should be carefully adjusted cause is is dependent on the actual trim of the vehicle,
-    for this reason a good working value can be derived from the vehicle's weight and leaving the extra corrections
-    to the controller subsystem in terms of gains and offsets.
+The buoyancy term should be carefully adjusted cause is is dependent on the actual trim of the vehicle,
+for this reason a good working value can be derived from the vehicle's weight and leaving the extra corrections
+to the controller subsystem in terms of gains and offsets.
 
-    For simulation purposes the buoyancy it can be computed using formula:
-      B = rho * volume * 9.81    # buoyancy (N)
+For simulation purposes the buoyancy it can be computed using formula:
+  B = rho * volume * 9.81    # buoyancy (N)
 
-    An early experiment measured this value to be 546.1106 for Nessie VII AUV but that value was depending on the
-    specific setup and trimming related to the experimental setup used for its measurement.
+An early experiment measured this value to be 546.1106 for Nessie VII AUV but that value was depending on the
+specific setup and trimming related to the experimental setup used for its measurement.
 
-    The COG and COB need a similar approach, in this case, they can be assumed to be in the same geometrical position
-    in order to avoid the generation of strong pitch forces when using this model with the actual vehicle. These terms
-    instead can be safely adjusted for simulation purposes.
+The COG and COB need a similar approach, in this case, they can be assumed to be in the same geometrical position
+in order to avoid the generation of strong pitch forces when using this model with the actual vehicle. These terms
+instead can be safely adjusted for simulation purposes.
 """
+
 from __future__ import division
 
 import numpy as np
