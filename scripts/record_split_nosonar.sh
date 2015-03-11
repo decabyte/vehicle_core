aa2aa4f744da95e3aa6c116b390f082e9e69d0d2
Valerio De Carolis <valerio.decarolis@gmail.com>
2015-03-11 12:12:47 +0000

    added scripts for vehicle operations (recording and time sync)

---
+++ scripts/record_split_nosonar.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# vars
+PREFIX=$1
+SIZE=$2
+OPTS='-x "bvt_MB2250(.*)|bvt_P900(.*)"'
+
+# usage
+print_usage() {
+    echo "Usage: $0 <prefix> [<size>]"
+    echo "Record all messages into a splitted .bag file with the same <prefix>"
+    echo ""
+    echo "Mandatory arguments:"
+    echo "  <prefix>: meanining full name of the bag files set"
+    echo ""
+    echo "Optional arguments:"
+    echo "  <size>: size in MB of the single chunks [default: 2048]"
+}
+
+# script body
+if [[ ! -n $1 ]]; then
+    print_usage
+    exit 1
+fi
+
+if [[ ! -n $2 ]]; then
+    SIZE=2048
+fi
+
+# info
+echo "Extracting data from bag files in $INPUT_DIR into csv files in $OUTPUT_DIR"
+echo "This can require some time, please wait ..."
+echo ""
+
+# recording
+echo "Starting the recording (please press Ctrl+C to terminate) ..."
+rosbag record -a --split --size=$SIZE -o $PREFIX $OPTS
+
+# send the last exit code
+exit $?
---
+++ scripts/sync_time_trieste.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+while true;
+do
+    ntpdate-debian 192.168.43.191
+    sleep 60
+done
+
