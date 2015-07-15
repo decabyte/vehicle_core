9dc4b997cb2a69682094595497373ef37b8f1559
Valerio De Carolis <valerio.decarolis@gmail.com>
2015-07-15 08:55:25 +0100

    adjusted path scripts for wave tank testing

--- tests/pp_fast_path.sh
+++ tests/pp_fast_path.sh
@@ -5,17 +5,16 @@ command: 'path'
 points:
 - values: [1.0, -1.8, 1.0, 0, 0, 0]
 - values: [4.4, -9.8, 1.0, 0, 0, 0]
-- values: [1.0, -9.8, 1.0, 0, 0, 0]
+- values: [1.0, -9.8, 1.0, 0, 0, 3.14]
 - values: [4.4, -1.8, 1.0, 0, 0, 0]
-- values: [1.0, -1.8, 1.0, 0, 0, 0]
+- values: [1.0, -1.8, 1.0, 0, 0, 3.14]
 options:
 - key: 'mode'
   value: 'fast'
 - key: 'interpolation_method'
   value: 'linear'
 - key: 'target_speed'
-  value: '0.4'
-
+  value: '0.60'
 - key: 'timeout'
   value: '300'
 """
--- tests/pp_lines_path.sh
+++ tests/pp_lines_path.sh
@@ -5,9 +5,9 @@ command: 'path'
 points:
 - values: [1.0, -1.8, 1.0, 0, 0, 0]
 - values: [4.4, -9.8, 1.0, 0, 0, 0]
-- values: [1.0, -9.8, 1.0, 0, 0, 0]
+- values: [1.0, -9.8, 1.0, 0, 0, 3.14]
 - values: [4.4, -1.8, 1.0, 0, 0, 0]
-- values: [1.0, -1.8, 1.0, 0, 0, 0]
+- values: [1.0, -1.8, 1.0, 0, 0, 3.14]
 options:
 - key: 'mode'
   value: 'lines'
