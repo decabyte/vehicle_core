66dfb90d5ef1df5190d535b2774d5530f02ce035
Trieste <hwu.osl@gmail.com>
2015-07-15 17:03:35 +0100

    fix from wavetank of config files

--- tests/pp_lawn_path.sh
+++ tests/pp_lawn_path.sh
@@ -4,11 +4,11 @@ rostopic pub -1 /path/request vehicle_interface/PathRequest """
 command: 'path'
 points:
 - values: [2.0, -2.0, 1.0, 0, 0, 0]
-- values: [6.0, -2.0, 1.0, 0, 0, 0]
-- values: [6.0, -6.0, 1.0, 0, 0, 0]
+- values: [4.0, -2.0, 1.0, 0, 0, 0]
+- values: [4.0, -6.0, 1.0, 0, 0, 0]
 - values: [2.0, -6.0, 1.0, 0, 0, 0]
 - values: [2.0, -10.0, 1.0, 0, 0, 0]
-- values: [6.0, -10.0, 1.0, 0, 0, 0]
+- values: [4.0, -10.0, 1.0, 0, 0, 0]
 options:
 - {key: 'mode', value: 'fast'}
 - {key: 'target_speed', value: '0.5'}
